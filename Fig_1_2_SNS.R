7b61a81f219cf1307449cd2b2b674bb9c405bfbd
CommunityHealth <76817397+creatingCommunityHealth@users.noreply.github.com>
2022-11-03 19:42:01 +0000

    Update Fig_1_2_SNS.R

--- Fig_1_2_SNS.R
+++ Fig_1_2_SNS.R
@@ -16,9 +16,9 @@ p <- p + geom_circle(aes(x0 = 25, y0 = 30, r = 22.92), fill = "#008080", alpha =
 p <- p + geom_circle(aes(x0 = 0, y0 = 15, r = 5.64), fill = "#008080", alpha = 0.2) 
 
 p <- p + annotate(geom = "text", x = 65, y = 34.5, family="Times", label = "Twitter", size = 6, colour = "black")
-p <- p + annotate(geom = "text", x = 65, y = 31, family="Times", label = "(320m)", size = 5, colour = "black")
+p <- p + annotate(geom = "text", x = 65, y = 31, family="Times", label = "(320mn)", size = 5, colour = "black")
 p <- p + annotate(geom = "text", x = 56.5, y = 14, family="Times", label = "Instagram", size = 6, colour = "black")
-p <- p + annotate(geom = "text", x = 56.5, y = 10.5, family="Times", label = "(400m)", size = 5, colour = "black")
+p <- p + annotate(geom = "text", x = 56.5, y = 10.5, family="Times", label = "(400mn)", size = 5, colour = "black")
 p <- p + annotate(geom = "text", x = 25, y = 30, family="Times", label = "Facebook", size = 7, colour = "black")
 p <- p + annotate(geom = "text", x = 25, y = 26.5, family="Times", label = "(1.65bn)", size = 6, colour = "black")
 p <- p + annotate(geom = "text", x = 0, y = 15.5, family="Times", label = "Snapchat", size = 5, colour = "black")
